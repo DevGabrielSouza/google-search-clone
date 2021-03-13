ce94cae93872223bb1213d4da9b78c8f13d6bc2b
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-13 20:30:13 -0300

    create search input state and add onChange property to input

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './style.css';
 
+import { useState } from 'react';
+
 //Icons
 import SearchIcon from '@material-ui/icons/Search';
 import KeyboardIcon from '@material-ui/icons/Keyboard';
 import MicIcon from '@material-ui/icons/Mic';
 
 function SearchInput() {
+
+    const [search, setSearch] = useState('');
+
     return (
         <div className="searchInput">
             <SearchIcon className="searchInput__icon" />
-            <input type="text" placeholder="Type here..." />
+            <input type="text" onChange={e => setSearch(e.target.value)} placeholder="Type here..." />
             <KeyboardIcon className="searchInput__icon" />
             <MicIcon className="searchInput__icon" />
         </div>
