e96559d568a0f3bf352698760a867f8531b47b57
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-13 19:35:25 -0300

    Create Search component and import searchInput inside

---
+++ src/components/Search/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './style.css';
+
+// Components
+import SearchInput from '../SearchInput'
+
+function Search() {
+    return (
+        <div>
+            <SearchInput />
+        </div>
+    )
+}
+
+export default Search
