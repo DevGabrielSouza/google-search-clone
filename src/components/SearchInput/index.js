56ff743267fbac7d58eab345b8a975f18545116a
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-13 19:28:06 -0300

    Create searchInput html structure

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './style.css';
 
+//Icons
+import SearchIcon from '@material-ui/icons/Search';
+import KeyboardIcon from '@material-ui/icons/Keyboard';
+import MicIcon from '@material-ui/icons/Mic';
+
 function SearchInput() {
     return (
         <div className="searchInput">
+            <SearchIcon className="searchInput__icon" />
             <input type="text" placeholder="Type here..." />
+            <KeyboardIcon className="searchInput__icon" />
+            <MicIcon className="searchInput__icon" />
         </div>
     )
 }
