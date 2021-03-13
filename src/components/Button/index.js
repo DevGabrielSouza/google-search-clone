995f4c4f92b00c5a0791933aa707df653e49c384
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-13 19:44:56 -0300

    Create Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css';
+
+function Button() {
+    return (
+        <button className="button">
+            
+        </button>
+    )
+}
+
+export default Button
