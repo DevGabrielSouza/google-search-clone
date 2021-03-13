7529a24156c73a023cb84fd5c0dd046380863f22
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-13 18:10:47 -0300

    Create home page header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import './style.css';
 
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div className="home">
             
             <div className="home__header">
+
+                <div className="home__headerLeft">
+
+                    <Link to="about">
+                        About
+                    </Link>
+
+                    <Link to="about">
+                        Store
+                    </Link>
+
+                </div>
+
+                <div className="home__headerRight">
+
+                    <Link to="about">
+                        Gmail
+                    </Link>
+
+                    <Link to="about">
+                        Images
+                    </Link>
+
+                </div>
 
             </div>
 
