ee3876e7aacbed81bcbc545b66ae307931cbff41
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-13 19:38:19 -0300

    Add class search to Search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -7,8 +7,12 @@ import SearchInput from '../SearchInput'
 
 function Search() {
     return (
-        <div>
+        <div className="search">
+
             <SearchInput />
+
+            
+
         </div>
     )
 }
