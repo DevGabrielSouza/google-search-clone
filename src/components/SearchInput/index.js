1007257318bff701a1bbc8e5a846811808c88744
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-13 19:08:06 -0300

    Create Search Input component

---
+++ src/components/SearchInput/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchInput() {
+    return (
+        <div className="searchInput">
+            <input type="text" placeholder="Type here..." />
+        </div>
+    )
+}
+
+export default SearchInput
