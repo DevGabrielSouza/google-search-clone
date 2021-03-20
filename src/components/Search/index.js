f51b0744cb10356b3f45c566bcbb820db5fcec15
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-20 16:10:44 -0300

    Create search function and call it on search button

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import axios from 'axios';
+
+import { useHistory  } from 'react-router-dom';
 
 import './style.css';
 
@@ -6,26 +9,52 @@ import './style.css';
 import SearchInput from '../SearchInput'
 import Button from './../Button/index';
 
+// Context
+import SearchContext from './context.js';
+
 function Search() {
+
+    const { searchTerm, setSearchResults} = useContext(SearchContext);
+    const history = useHistory();
+
+    function getSearchResults(e){
+
+        
+
+        const googleCustomSearchApiUrl = `https://www.googleapis.com/customsearch/v1?key=AIzaSyBOiAGGbzofGZVls20Ubvh599b0cMvutfc&cx=65763cfd0eb1c2db2&q=${searchTerm}`
+
+
+        axios.get(googleCustomSearchApiUrl)
+        .then(function(response){
+
+            setSearchResults(response.data)
+
+            history.push(`/search?q=${searchTerm}`);
+
+        })
+        
+    }
+
     return (
-        <div className="search">
 
-            <SearchInput />
+            <div className="search">
 
-            <div className="search__buttons">
+                <SearchInput />
 
-                <Button>
-                    Pesquisa Google
-                </Button>
+                <div className="search__buttons">
 
-                <Button>
-                    Estou com sorte
-                </Button>
+                    <Button getSearchResults={getSearchResults}>
+                        Pesquisa Google
+                    </Button>
+
+                    <Button>
+                        Estou com sorte
+                    </Button>
+
+                </div>
+
 
             </div>
-
-
-        </div>
     )
 }
 
