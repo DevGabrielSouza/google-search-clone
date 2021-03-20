426ea11ababa9124bb188dd091266395efe6d350
Gabriel Souza <contato@devgabrielsouza.com.br>
2021-03-20 16:14:37 -0300

    Add SearchList component, passing results data to render Item

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,19 +1,44 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import SearchList from '../../components/SearchList/index.js';
+import './style.css';
+
+import axios from 'axios';
 
 // Context
 import SearchContext from './../../components/Search/context.js';
 
 export default function Search() {
 
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const searchedTerm = urlParams.get('q');
+
     const { searchResults } = useContext(SearchContext);
 
-    const searchResultItems = searchResults.items;
+    const [searchResultItems, setSearchResultItems] = useState([]);
 
-    console.log(searchResults);
+    useEffect(() => {
+        if( searchResults.items && searchResults.items > 0 ){
+        
 
+            setSearchResultItems(searchResults.items);
+    
+            
+        }else{
+            const googleCustomSearchApiUrl = `https://www.googleapis.com/customsearch/v1?key=AIzaSyBOiAGGbzofGZVls20Ubvh599b0cMvutfc&cx=65763cfd0eb1c2db2&q=${searchedTerm}`
+    
+    
+            axios.get(googleCustomSearchApiUrl)
+            .then(function(response){
+                setSearchResultItems(response.data.items)
+    
+            })
+        }
+    }, [searchedTerm])
+    
     return (
-        <div>
-            search pagea
+        <div className="searchPage">
+            <SearchList items={searchResultItems} />
         </div>
     )
 }
